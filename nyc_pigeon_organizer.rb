b8826c8d8738a34dce4ec2827bb9004128bc0393
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-19 02:17:13 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         else
           pigeon_list[element] = {key1 => [key2.to_s]}
         end
-        binding.pry
+        #binding.pry
       end
     end
   end
