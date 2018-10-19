fea8ab160772c8daf5a98ac023ca3058a804ca16
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-19 01:53:36 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key1, value1|
     #key1 == :color, value1 == {:purple=>["Theo", "Peter Jr.", "Lucky"], :grey=>["Theo", "Peter Jr.", "Ms. K"], :white=>["Queenie", "Andrew", "Ms. K", "Alex"], :brown=>["Queenie", "Alex"]}
     
-    types.each do |key2, value2|
+    value1.each do |key2, value2|
       #key2 == :purple, value2 == ["Theo", "Peter Jr.", "Lucky"]
       binding.pry
       value2.each do |element|
