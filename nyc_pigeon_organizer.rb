80f8c700ae3da5f120a16a93942156eb373b5307
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-19 01:38:12 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attributes, types|
+    #attributes == :color, types == {:purple=>["Theo", "Peter Jr.", "Lucky"], :grey=>["Theo", "Peter Jr.", "Ms. K"], :white=>["Queenie", "Andrew", "Ms. K", "Alex"], :brown=>["Queenie", "Alex"]}
+    
+    types.each do |key, value|
+      #key == :purple, value == ["Theo", "Peter Jr.", "Lucky"]
+      value.each do |name|
+        if pigeon_list.has_key?(name)
+    end
+  end
 end
