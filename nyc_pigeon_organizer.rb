cf4c27e8211c331653abe6d1ed6afef41e27f1ac
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-19 02:12:48 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,17 +4,14 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |key1, value1|
-    #key1 == :color, value1 == {:purple=>["Theo", "Peter Jr.", "Lucky"], :grey=>["Theo", "Peter Jr.", "Ms. K"], :white=>["Queenie", "Andrew", "Ms. K", "Alex"], :brown=>["Queenie", "Alex"]}
-    
     value1.each do |key2, value2|
-      #key2 == :purple, value2 == ["Theo", "Peter Jr.", "Lucky"]
       value2.each do |element|
         if pigeon_list.has_key?(element)
           pigeon_list[element] = {key1 => [key2.to_s]}
         else
-          pigeon_list[element][key1].push(key2.to_s)
+          binding.pry
+          pigeon_list[element][key1] << key2.to_s
         end
-        binding.pry
       end
     end
   end
