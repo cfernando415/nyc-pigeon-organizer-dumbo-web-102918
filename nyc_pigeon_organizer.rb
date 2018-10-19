1fdf7a79a025c02af77dfef1d06e764425d9a111
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-19 02:10:14 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,11 @@ def nyc_pigeon_organizer(data)
     value1.each do |key2, value2|
       #key2 == :purple, value2 == ["Theo", "Peter Jr.", "Lucky"]
       value2.each do |element|
-        pigeon_list[element] = {key1 => [key2.to_s]}
+        if pigeon_list.has_key?(element)
+          pigeon_list[element] = {key1 => [key2.to_s]}
+        else
+          pigeon_list[element][key1] << key2.to_s
+        end
         binding.pry
       end
     end
